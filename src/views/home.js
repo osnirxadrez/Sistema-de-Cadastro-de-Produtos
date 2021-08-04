ee9bb599eda7800e37656cc0a44d244ed0b992aa
Osnir Basílio <osnirxadrez@gmail.com>
2021-08-03 23:07:59 -0300

    Feito o menu, tela Home e tela de cadastro de produtos

---
+++ src/views/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div class="jumbotron">
+            <h1 class="display-3">Bem Vindo!</h1>
+            <p class="lead">Este é seu sistema de cadastro de produtos!.</p>
+            <hr class="my-4"/>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Cadastar</a>
+                </p>
+        </div>
+    )
+}
+
+export default Home;
